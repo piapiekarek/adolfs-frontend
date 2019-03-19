639beddbb7bd86f6ef325881e983203a89d56586
Pia Piekarek <piapiekarek@googlemail.com>
2019-03-19 12:05:53 +0100

    bug fix

--- src/page/machines/MachineView.js
+++ src/page/machines/MachineView.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import MachineItem from './MachineItem';
 import '../../css/machines.css';
 
